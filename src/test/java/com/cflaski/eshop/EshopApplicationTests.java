eac142bdd1adbfe088c36156efdbc19b9c219196
christina <xrflas@gmail.com>
2025-11-16 19:31:07 +0200

    feat: initial commit

---
+++ src/test/java/com/cflaski/eshop/EshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cflaski.eshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
